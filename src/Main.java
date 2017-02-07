28ef50cca95b44db34b21ab30040d8baf65b2c3e
Julie Schneider <julieschneider@Julies-MacBook-Pro.local>
2017-02-07 08:52:14 -0500

    lab8

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,9 +9,8 @@ public class Main {
         int j;
         int i;
         double sum = 0;
-        double average;
         double hits = 0;
-        double percentage;
+        String userInput;
 
         int[][] atBat = new int[1][]; //jagged array
         //jagged array has different number of columns for each row
@@ -18,6 +18,28 @@ public class Main {
         System.out.println("Welcome to the Batting Average Calculator!");
         System.out.println();
 
+        do {
+
+        atBatMath(scan, sum, hits, atBat);
+
+            System.out.println();
+            System.out.println("Would you like to try another batter? yes/no");
+            scan.nextLine();
+            userInput = scan.nextLine();
+            scan.nextLine();
+
+        } while (userInput.equalsIgnoreCase("yes"));
+
+        System.out.println("Bye!");
+
+    }
+
+    public static void atBatMath(Scanner scan, double sum, double hits, int[][] atBat) {
+
+        int i;
+        int j;
+        double percentage;
+        double average;
         for (i = 0; i < atBat.length; i++) {
 
             System.out.println("Please enter a number of times at bat: "); //collects the number of columns
@@ -31,44 +53,55 @@ public class Main {
 
             for (j = 0; j < atBat[i].length; j++) {
 
-                    System.out.println("Enter result for at-bat: ");
-                    atBat[i][j] = rangeValidator();
+                System.out.println("Enter result for at-bat: ");
+                atBat[i][j] = rangeValidator();
 
-                   if (atBat[i][j] != 0) {
-                       hits++;
-                   }
+                hits = getHits(atBat[i][j], hits);
 
-                   sum = sum + atBat[i][j];
+                sum = sum + atBat[i][j];
 
-                }
+            }
 
-                percentage = sum / bat;
-                average = hits / bat;
+            percentage = sum / bat;
+            average = hits / bat;
 
             System.out.println();
             System.out.println("The batting average is: " + average);
             System.out.println();
             System.out.println("The slugging percentage is: " + percentage);
 
-            }
+            //gathers info for each hit, calculates sum and hits
+            //calculates percentage and average
+            //prints out values
 
         }
+    }
 
+    public static double getHits(int i, double hits) {
 
-        public static int rangeValidator() {
-
-            Scanner scan = new Scanner(System.in);
-
-            int input = scan.nextInt();
-
-            while(input < 0 || input > 4 ) {
-
-                System.out.println("Please enter a number between 0 and 4!");
-                input = scan.nextInt();
-            }
-
-            return input;
-
+        if (i != 0) {
+            hits++;
         }
+
+        return hits;
+
+        //math for adding up how many hits
+    }
+
+    public static int rangeValidator() {
+
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        while (input < 0 || input > 4) {
+
+            System.out.println("Please enter a number between 0 and 4!");
+            input = scan.nextInt();
+        }
+
+        return input;
+
+    }
 
 }
